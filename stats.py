df7210364eef187cfe1a06779a86411c35b5d2c1
Hebbar <prajwalhebbaras@gmail.com>
2025-06-12 20:51:44 +0530

    refactor: update character count function to use the built in function

--- stats.py
+++ stats.py
@@ -1,7 +1,13 @@
+from collections import Counter
+
+
 def get_words_count(file_str):
     number_of_words = file_str.split()
     return len(number_of_words)
 
+
+"""
+Older function to count the number of characters using loops
 
 def get_character_count(file_str):
     lowercase_file_str = file_str.lower()
@@ -12,5 +18,15 @@ def get_character_count(file_str):
             character_count[char] += 1
         else:
             character_count[char] = 1
+
+    return character_count
+"""
+
+# Count the number of characters using collections counter built in package
+
+
+def get_character_count(file_str):
+    lowercase_file_str = file_str.lower()
+    character_count = Counter(lowercase_file_str)
 
     return character_count
