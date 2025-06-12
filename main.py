720bd6a980af5acfe67d166f237b6405f2bf10c1
Hebbar <prajwalhebbaras@gmail.com>
2025-06-12 20:45:09 +0530

    feature: update the character count function

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-from stats import get_words_count
+from stats import get_words_count, get_character_count
 from read_book import get_book_text
 
 
 def main():
     book_contents = get_book_text("books/frankenstein.txt")
     word_count = get_words_count(book_contents)
+    character_count = get_character_count(book_contents)
     print(f"{word_count} words found in the document")
+    print(f"Chacter Count {character_count}")
 
 
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,16 @@
 def get_words_count(file_str):
     number_of_words = file_str.split()
     return len(number_of_words)
+
+
+def get_character_count(file_str):
+    lowercase_file_str = file_str.lower()
+    character_count = {}
+
+    for char in lowercase_file_str:
+        if char in character_count:
+            character_count[char] += 1
+        else:
+            character_count[char] = 1
+
+    return character_count
