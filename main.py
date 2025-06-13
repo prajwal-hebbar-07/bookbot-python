97e5dc26d98a903405825e2b6c42b3165da36c90
Hebbar <prajwalhebbaras@gmail.com>
2025-06-13 17:17:19 +0530

    update command line argument usage in the main functio

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 from stats import get_words_count, get_character_count
 from read_book import get_book_text
+from sys import argv, exit
 
 
 def main():
-    book_title = "frankenstein.txt"
-    book_contents = get_book_text(f"books/{book_title}")
+    if len(argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        exit(1)
+
+    book_title = argv[1]
+    book_contents = get_book_text(book_title)
     word_count = get_words_count(book_contents)
     character_count = get_character_count(book_contents)
 
     print("============ BOOKBOT ============")
-    print(f"Analyzing book found at books/{book_title}...")
+    print(f"Analyzing book found at {book_title}...")
     print("----------- Word Count ----------")
     print(f"Found {word_count} total words")
     print("--------- Character Count -------")
